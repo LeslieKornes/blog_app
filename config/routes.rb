919e85e1bd83102710af0da47b252cf2c025e2ee
Leslie Kornes <lesliekornes@gmail.com>
2018-02-04 07:03:22 -0500

    added new article to new action and built out form in new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+resources :articles
+
 get 'pages/home', to: 'pages#home'
 
 get 'pages/about', to: 'pages#about'
 
 #following line provides all CRUD routes (check using- rails routes)
-resources :articles
+
 
 end
