fb8b0d38c9a93de101e2f2fc1b6e5894d0a4cd67
Leslie Kornes <lesliekornes@gmail.com>
2018-02-11 23:23:10 -0500

    added fk association bt user and articles and added debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,9 @@ before_action :set_article, only: [:edit, :update, :show, :destroy]
   end
 
   def create
-    #render plain: params[:article].inspect
+    #debugger
     @article = Article.new(article_params)
+    @article.user = User.find(2)
     if @article.save
       flash[:success] = "Article was successfully created."
       redirect_to article_path(@article)
