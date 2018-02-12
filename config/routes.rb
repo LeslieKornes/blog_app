2e8a635247214d9ea2a01e8e687a92f3c25199ce
Leslie Kornes <lesliekornes@gmail.com>
2018-02-12 00:27:54 -0500

    Added Users Controller and signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-resources :articles
-
 root 'pages#home'
 
 get 'about', to: 'pages#about'
 
-#following line provides all CRUD routes (check using- rails routes)
+resources :articles
 
+get 'signup', to: 'users#new'
 
 end
