aacc82249e5f1dc2c911affa8db80d28652045cf
Leslie Kornes <lesliekornes@gmail.com>
2018-02-14 11:31:11 -0500

    Added login/sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to users_path(user)
+    else
+      flash.now[:danger] = "There was an error with your login information"
+      render 'new'
+    end
   end
 
   def destroy
