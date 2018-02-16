ad7eb98508e2cef8c117561d35b098de43d10d79
Leslie Kornes <lesliekornes@gmail.com>
2018-02-15 20:15:08 -0500

    Built out category model using tdd

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -10,4 +10,25 @@ class CategoryTest < ActiveSupport::TestCase
     assert @category.valid?
   end
 
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    cat2 = Category.new(name: "sports")
+    assert_not cat2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
 end
